ebcba04313b85fd82d17a67fc920709eb819dd44
brtz1 <gustavo.ismerio@gmail.com>
2025-07-05 14:15:16 -0700

    Fixing errors on Docker

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -2,19 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-// Routes
-import teamRoutes from './routes/teamRoutes';
-
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// API routes
-app.use('/api/teams', teamRoutes);
-
-// Health check
 app.get('/api/health', (req, res) => {
   res.json({ status: 'API is running!' });
 });
